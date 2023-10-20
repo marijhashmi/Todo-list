ee7c8b8f99c5ae93178c876d8ec07fa1b063d42e
marijhashmi <143936611+marijhashmi@users.noreply.github.com>
2023-10-21 04:09:58 +0500

    Add files via upload

---
+++ todo.ts
@@ -0,0 +1,46 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+import chalkAnimation from "chalk-animation";
+import { log } from "console";
+
+let todos:string[]=[];
+let loop=true;
+while(loop){
+const answer:{
+todo:string,
+retry:boolean
+}=await inquirer.prompt([{
+    type:'input',
+    name:'todo',
+    message:'Enter the todo list: '
+
+},
+{
+    type:'confirm',
+    name:'retry',
+    message:'Again Enter todo list : ',
+    default:false
+
+}
+
+])
+const {todo,retry}=answer;
+loop=retry;
+if(todo){
+   todos.push(todo);
+}
+else{
+    console.log(chalk.redBright("Enter Invalid Input!"));
+    
+}
+
+}
+for (let i = 0; i < todos.length; i++) {
+console.log(chalk.blueBright("Your Todo list: \n"));
+console.log(`${i+1}. ${todos[i]}\n`);
+if(todos.length<0)
+{
+    console.log(chalk.red(`You have no tasks in your to do list!`));
+    
+}
+}
